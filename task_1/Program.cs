a70e8b4030ae2a1d7fba546d86ebd1f6cb39b59c
blazhkevych.v <blazhkevych.v@gmail.com>
2022-09-28 17:26:18 +0300

    Добавьте файлы проекта.

---
+++ task_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task_1
+{
+    internal class Program
+    {
+        /*
+         1. Пользователь с клавиатуры вводит в строку арифметическое
+           выражение. Приложение должно посчитать его результат.
+           Необходимо поддерживать только две операции: «+» и «–».
+         */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите арифметическое выражение (числа и операции через пробел): ");
+            string? str = Console.ReadLine();
+            //string str = "-3 + 32 + -2 - 5 + 11 + -4";
+
+            string[] arrayOfString = str.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            double result = 0, num1 = 0;
+            bool plus = true;
+            for (int i = 0; i < arrayOfString.Length; i++)
+            {
+                if (double.TryParse(arrayOfString[i], out num1))
+                {
+                    if (plus)
+                        result = result + num1;
+                    else
+                        result = result - num1;
+                }
+                else
+                    plus = arrayOfString[i] == "+";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace task_2
+{
+    internal class Program
+    {
+        /*
+         2. Пользователь с клавиатуры вводит некоторый текст. Приложение
+           должно изменять регистр первой буквы каждого предложения на
+           букву в верхнем регистре. Например, если пользователь ввёл: «today
+           is a good day for walking. i will try to walk near the sea». Результат работы
+           приложения: «Today is a good day for walking. I will try to walk near the sea».
+         */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите арифметическое выражение (числа и операции через пробел): ");
+            string? str = Console.ReadLine();
+            //string str = "today is a good day for walking. i will try to walk near the sea.";
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(str[0].ToString().ToUpper());
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]) && char.IsWhiteSpace(str[i - 1]) && ".!?".IndexOf(str[i - 2]) != -1)
+                {
+                    stringBuilder.Append(str[i].ToString().ToUpper());
+                }
+                else
+                {
+                    stringBuilder.Append(str[i]);
+                }
+            }
+            Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}
